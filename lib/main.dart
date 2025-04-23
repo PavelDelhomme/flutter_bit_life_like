c638172f81ebe2f072eea1adeddfc2c9577fb93b
Paul Delhomme <dev@delhomme.ovh>
2025-04-23 22:12:37 +0200

    Touujours pas d'affichage des plugin correctmenet ni possibilité d'enregistrer les personnage et charger leur vie et leur monde

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
-import 'package:bitlife_like/core/services/game_state_service.dart';
-import 'package:bitlife_like/plugin_manager.dart';
-import 'package:bitlife_like/plugins/core_plugins/assets_extended/models/adapters/antique.adapter.dart';
-import 'package:bitlife_like/plugins/core_plugins/assets_extended/models/adapters/arme.adapter.dart';
-import 'package:bitlife_like/plugins/core_plugins/assets_extended/models/adapters/assets.adapter.dart';
-import 'package:bitlife_like/plugins/core_plugins/assets_extended/models/adapters/jewelry.adapter.dart';
-import 'package:bitlife_like/plugins/core_plugins/assets_extended/models/adapters/real_estate.adapter.dart';
-import 'package:bitlife_like/plugins/core_plugins/assets_extended/models/adapters/vehicle.adapter.dart';
 import 'package:flutter/material.dart';
+
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_flutter/adapters.dart';
+
+import 'core/models/character.dart';
+import 'plugin_manager.dart';
+import 'plugins/core_plugins/assets_extended/models/adapters/antique.adapter.dart';
+import 'plugins/core_plugins/assets_extended/models/adapters/arme.adapter.dart';
+import 'plugins/core_plugins/assets_extended/models/adapters/assets.adapter.dart';
+import 'plugins/core_plugins/assets_extended/models/adapters/jewelry.adapter.dart';
+import 'plugins/core_plugins/assets_extended/models/adapters/real_estate.adapter.dart';
+import 'plugins/core_plugins/assets_extended/models/adapters/vehicle.adapter.dart';
+
+import 'core/services/game_state_service.dart';
 import 'core/models/character.adapter.dart';
+import 'core/plugin/game_plugin_context.dart';
 import 'core/services/data_service.dart';
 import 'core/services/legal_service.dart';
+import 'core/services/save_manager.dart';
 import 'core/services/skill_tree_manager.dart';
 import 'core/shared/adapters/bank_account.adapter.dart';
 import 'core/shared/legal.dart';
@@ -19,10 +26,10 @@ import 'core/ui/start_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
 
   // Initialisation Hive
   Hive
-    ..initFlutter()
     ..registerAdapter(CharacterAdapter())
     ..registerAdapter(AssetAdapter())
     ..registerAdapter(AntiqueAdapter())
@@ -30,8 +37,9 @@ void main() async {
     ..registerAdapter(RealEstateAdapter())
     ..registerAdapter(JewelryAdapter())
     ..registerAdapter(VehicleAdapter())
-    ..registerAdapter(BankAccountAdapter())
-  ;
+    ..registerAdapter(BankAccountAdapter());
+
+  await SaveManager.initialize();
 
   // Préchargement des données
   await DataService.preloadCities();
@@ -41,14 +49,32 @@ void main() async {
   // Initialisation des services
   LegalService.initialize(await LegalSystem.loadDefaultSystems());
 
-  runApp(const BitLifeApp());
+  final character = await SaveManager.loadMainCharacter(); // ou en dur
+
+  runApp(BitLifeApp(initialCharacter: character));
 }
 
 class BitLifeApp extends StatelessWidget {
-  const BitLifeApp({super.key});
+  final Character? initialCharacter;
+
+  const BitLifeApp({super.key, required this.initialCharacter});
 
   @override
   Widget build(BuildContext context) {
+    if (initialCharacter != null) {
+      GameStateService.instance.setCharacter(initialCharacter!);
+      GameStateService.instance.initializeWorld();
+
+      PluginManager.instance.registerAll();
+      PluginManager.instance.startGamePlugins(
+        GamePluginContext(
+          mainCharacter: GameStateService.instance.character,
+          gameState: GameStateService.instance,
+          eventService: GameStateService.instance.eventService,
+        ),
+      );
+    }
+
     return MaterialApp(
       title: 'BitLife Clone',
       theme: ThemeData(
